045eb7a7553b66f3efdaafb15793ebc2dc2a41bf
Lev Noikas <noikaslev@gmail.com>
2014-10-23 23:07:43 +0300

    save to mongolab, read tags from file

--- footballTeams.js
+++ footballTeams.js
@@ -1,41 +1,72 @@
-var fs = require('fs');
-
 var Twitter = require('./')
-  , t = new Twitter({
-    consumer_key: 'qzdhyEANdQOCXY6zsixwRWoXy',
-    consumer_secret: '4nzU3uzxqJHgXRr8ikXeFeX2HCla8AEiLwkvTEdckff0b6oQMv',
-    token: '2865550398-bSHwf7aKqlzrookfMy2XhcCSYEQvrf0mxG7O7rf',
-    token_secret: 't1yURKhN8QnVwtJWaW3quxVZV8g9Efk488zxhVJloqbNU'
-  })
+    , mongoose = require('mongoose')
+    , Tweet = require('./lib/models/tweet-model')
+    , fs = require('fs')
+    , t = new Twitter({
+        consumer_key: 'qzdhyEANdQOCXY6zsixwRWoXy',
+        consumer_secret: '4nzU3uzxqJHgXRr8ikXeFeX2HCla8AEiLwkvTEdckff0b6oQMv',
+        token: '2865550398-bSHwf7aKqlzrookfMy2XhcCSYEQvrf0mxG7O7rf',
+        token_secret: 't1yURKhN8QnVwtJWaW3quxVZV8g9Efk488zxhVJloqbNU'
+    });
+
+
+mongoose.connect('mongodb://admin:Abcd1234@ds047050.mongolab.com:47050/football');
+
 
 t.on('tweet', function (tweet) {
-    data = {tweetData:{ tweetID: tweet.id,
-                        lang: tweet.lang,
-                        retweeted: tweet.retweeted,
-                        retweetedCount: tweet.retweet_count,
-                        timestamp: tweet.timestamp_ms,
-                        place: tweet.place,
-                        source: tweet.source
-           },
-            userID:{ userID: tweet.user.id,
-                      followers: tweet.user.followers_count
-             }
-    };
-
-  console.log(data);
-       // location, hasgtags, text, retweeted, retweet_count, source(iPhone, Android)
-
-})
+    saveTweet(tweet);
+    // console.log(tweet.text);
+});
 
 t.on('error', function (err) {
-  console.log('Oh no')
-})
+    console.log('Oh no')
+});
 
 t.on('reconnect', function (msg) {
-  console.log(msg)
-})
+    console.log(msg)
+});
 
-t.track('#Liverpool', false)
+var startTracking = function(){
+    fs.readFile('./TeamsList.txt',  function (err, data) {
+        if (err) throw err;
+        var bufferString = data.toString();
+        var bufferStringSplit = bufferString.split('\n');
 
-//t.track('@cnet', false)
-t.reconnect()
+        for(tag in bufferStringSplit){
+            t.track(bufferStringSplit[tag], false);
+        };
+
+        t.reconnect();
+    });
+};
+
+
+startTracking();
+
+
+var saveTweet = function(tweet){
+    data = {tweetData:{ tweetID: tweet.id,
+        lang: tweet.lang,
+        retweeted: tweet.retweeted,
+        retweetedCount: tweet.retweet_count,
+        timestamp: tweet.timestamp_ms,
+        place: tweet.place,
+        source: tweet.source,//(iPhone, Android, web)
+        content: tweet.text
+    },
+        userID:{ userID: tweet.user.id,
+            followers: tweet.user.followers_count
+        }
+    };
+
+    var monTweet = new Tweet(data);
+    monTweet.save(function (err, document) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log("Saved document: " + document);
+    });
+};
+
